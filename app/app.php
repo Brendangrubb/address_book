a2315f640fa6e61658c113903a211105eb4b4489
Brendan Grubb <brendangrubb@gmail.com>
2017-02-10 08:26:40 -0800

    link twig to app.php

--- app/app.php
+++ app/app.php
@@ -4,10 +4,14 @@
     // require_once __DIR__.'/../src/Contact.php';
 
     $app = new Silex\Application();
+    $app->register(
+        new Silex\Provider\TwigServiceProvider(),
+        array('twig.path'=>__DIR__.'/../views')
+    );
 
-    $app->get('/', function() {
-        return "<p>Linked!</p>";
+    $app->get('/', function() use ($app){
 
+        return $app['twig']->render('address_book_home.html.twig');
     });
 
     return $app;
