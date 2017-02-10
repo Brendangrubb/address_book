c2bf0e0d839a519d51e366ad41701984861f352a
Brendan Grubb <brendangrubb@gmail.com>
2017-02-10 10:31:15 -0800

    add logic to save, display and delete user contacts

--- app/app.php
+++ app/app.php
@@ -3,6 +3,11 @@
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/Contact.php';
 
+    session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
     $app = new Silex\Application();
     $app['debug'] = true;
 
@@ -12,17 +17,26 @@
     );
 
     $app->get('/', function() use ($app){
+        $all_contacts = Contact::getAll();
 
-        return $app['twig']->render('address_book_home.html.twig');
+        return $app['twig']->render('address_book_home.html.twig', array('all_contacts' => $all_contacts));
     });
 
     $app->post('/add_a_contact', function() use ($app){
 
-        return $app['twig']->render('add_a_contact.html.twig');
+        $new_contact = new Contact(
+            $_POST['name'],
+            $_POST['number'],
+            $_POST['address']
+        );
+
+        $new_contact->save();
+
+        return $app['twig']->render('add_a_contact.html.twig', array('new_contact' => $new_contact));
     });
 
     $app->post('/delete_all_contacts', function() use ($app){
-
+        Contact::deleteAll();
         return $app['twig']->render('delete_all_contacts.html.twig');
     });
 
